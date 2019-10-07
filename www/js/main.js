52cc5307f7d65c3c5fef67282eae97a8477c5d85
bigreni <bigreni@gmail.com>
2019-10-07 12:06:09 -0500

    Version 2

--- www/js/main.js
+++ www/js/main.js
@@ -67,11 +67,11 @@
    function checkFirstUse()
     {
         $(".dropList").select2();
-        window.ga.startTrackerWithId('UA-88579601-19', 1, function(msg) {
-            window.ga.trackView('Home');
-        });  
         initApp();
         askRating();
+        //window.ga.startTrackerWithId('UA-88579601-19', 1, function(msg) {
+        //    window.ga.trackView('Home');
+        //});
         //document.getElementById("screen").style.display = 'none';     
     }
 
