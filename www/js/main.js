d81b764a206e84db7b12271f18796e37d6ff3908
bigreni <santhanakrishnan.sai@gmail.com>
2022-10-09 22:40:23 -0500

    Version 2.1

--- www/js/main.js
+++ www/js/main.js
@@ -82,10 +82,32 @@
         document.getElementById("screen").style.display = 'none';     
     }
 
+    function checkPermissions(){
+        const idfaPlugin = cordova.plugins.idfa;
+    
+        idfaPlugin.getInfo()
+            .then(info => {
+                if (!info.trackingLimited) {
+                    return info.idfa || info.aaid;
+                } else if (info.trackingPermission === idfaPlugin.TRACKING_PERMISSION_NOT_DETERMINED) {
+                    return idfaPlugin.requestPermission().then(result => {
+                        if (result === idfaPlugin.TRACKING_PERMISSION_AUTHORIZED) {
+                            return idfaPlugin.getInfo().then(info => {
+                                return info.idfa || info.aaid;
+                            });
+                        }
+                    });
+                }
+            });
+    }
+
 function askRating()
 {
-  AppRate.preferences = {
-  openStoreInApp: true,
+cordova.plugins.AppRate.setPreferences = {
+    reviewType: {
+        ios: 'AppStoreReview',
+        android: 'InAppBrowser'
+        },
   useLanguage:  'en',
   usesUntilPrompt: 10,
   promptAgainForEachNewVersion: true,
@@ -100,14 +122,14 @@ AppRate.promptForRating(false);
 
 function showAd()
 {
-    document.getElementById("screen").style.display = 'block'; 
-    if ((/(android|windows phone)/i.test(navigator.userAgent))) {
-        AdMob.isInterstitialReady(function(isready){
-            if(isready) 
-                AdMob.showInterstitial();
-        });
-    }
-    document.getElementById("screen").style.display = 'none'; 
+document.getElementById("screen").style.display = 'block';     
+if ((/(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent))) {
+    AdMob.isInterstitialReady(function(isready){
+        if(isready) 
+            AdMob.showInterstitial();
+    });
+}
+document.getElementById("screen").style.display = 'none'; 
 }
 
 function loadFaves()
